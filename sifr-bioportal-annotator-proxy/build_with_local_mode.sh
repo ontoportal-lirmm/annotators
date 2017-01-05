c1195f6a1c5f215d231a055564207706c7970649
twktheainur <twk.theainur@gmail.com>
2017-01-05 18:03:30 +0100

    Updated dependency version for icu4j because of an incompatibility with jetty 9 deployment

--- sifr-bioportal-annotator-proxy/build_with_local_mode.sh
+++ sifr-bioportal-annotator-proxy/build_with_local_mode.sh
@@ -1,3 +1,3 @@
-cp annotatorProxy.properties.sample sifr-bioportal-annotator-proxy/src/main/resources/annotatorProxy.properties
+cp annotatorProxy.properties.sample src/main/resources/annotatorProxy.properties
 mvn validate 
 mvn clean install
