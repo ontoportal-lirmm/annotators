a5b0b6e967f312e26b984a6e4e0c59e570996bed
Andon Tchechmedjiev <twk.theainur@gmail.com>
2018-10-09 22:50:32 +0200

    Refactoring of brat output, performance drop

--- sifr-bioportal-annotation-model/src/main/java/org/sifrproject/annotations/output/brat/EHealthQuaeroBratOutputGenerator.java
+++ sifr-bioportal-annotation-model/src/main/java/org/sifrproject/annotations/output/brat/EHealthQuaeroBratOutputGenerator.java
@@ -1,7 +1,7 @@
 package org.sifrproject.annotations.output.brat;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.sifrproject.annotations.api.input.AnnotationParser;
-import org.sifrproject.annotations.api.model.AnnotatedClass;
 import org.sifrproject.annotations.api.model.Annotation;
 import org.sifrproject.annotations.api.model.AnnotationToken;
 import org.sifrproject.annotations.api.model.ScoreableElement;
@@ -12,6 +12,7 @@ import org.sifrproject.annotations.output.MimeTypes;
 import org.sifrproject.annotations.umls.UMLSGroup;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Produces a BRAT output from a list of bioportal {@code {@link Annotation}} objects compatible with the
@@ -34,10 +35,10 @@ public class EHealthQuaeroBratOutputGenerator implements OutputGenerator {
     @Override
     public AnnotatorOutput generate(final List<Annotation> annotations, final String annotatorURI, final String sourceText) {
         final Map<AnnotationToken, List<Annotation>> perTokenAnnotations = AnnotationParser.perTokenAnnotations(annotations);
-        int termCounter = 0;
+        final List<QuaeroAnnotation> filteredAnnotations = new ArrayList<>();
+
         final StringBuilder stringBuilder = new StringBuilder();
         for (final Map.Entry<AnnotationToken, List<Annotation>> annotationTokenListEntry : perTokenAnnotations.entrySet()) {
-            termCounter++;
             List<Annotation> annotationsForToken = annotationTokenListEntry.getValue();
             annotationsForToken.sort(Comparator.comparingDouble(ScoreableElement::getScore));
 
@@ -54,7 +55,10 @@ public class EHealthQuaeroBratOutputGenerator implements OutputGenerator {
             if (!annotationsForToken.isEmpty()) {
                 Annotation current = annotationsForToken.get(0);
                 int currentIndex = 1;
-                while (current.getAnnotatedClass().getCuis().isEmpty() && (currentIndex < annotationsForToken.size())) {
+                while (current
+                        .getAnnotatedClass()
+                        .getCuis()
+                        .isEmpty() && (currentIndex < annotationsForToken.size())) {
                     current = annotationsForToken.get(currentIndex);
                     currentIndex++;
 
@@ -63,50 +67,132 @@ public class EHealthQuaeroBratOutputGenerator implements OutputGenerator {
             }
 
             if (annotation != null) {
-                final AnnotatedClass annotatedClass = annotation.getAnnotatedClass();
-                final Collection<UMLSGroup> semanticGroups = new HashSet<>(annotatedClass.getSemanticGroups());
-                String group = "";
-                group += (selectSingleGroup && !semanticGroups.isEmpty()) ? semanticGroups.iterator().next().getName() : buildGroupList(semanticGroups);
+                @SuppressWarnings("LocalVariableOfConcreteClass") final QuaeroAnnotation quaeroAnnotation = new QuaeroAnnotation(annotationTokenListEntry
+                        .getKey()
+                        .getFrom(), annotationTokenListEntry
+                        .getKey()
+                        .getTo(), annotationTokenListEntry
+                        .getKey()
+                        .getText());
+                quaeroAnnotation.addUMLSSemanticGroups(annotation
+                        .getAnnotatedClass()
+                        .getSemanticGroups());
+                quaeroAnnotation.addCUIs(annotation
+                        .getAnnotatedClass()
+                        .getCuis());
+                filteredAnnotations.add(quaeroAnnotation);
+            }
+        }
 
-                if(!semanticGroups.isEmpty()) {
-                    if(!ignoreAmbiguous || (ignoreAmbiguous && (semanticGroups.size() == 1))) {
-                        stringBuilder.append(String.format("T%d\t%s %d %d\t%s", termCounter, group,
-                                annotationTokenListEntry.getKey().getFrom(),
-                                annotationTokenListEntry.getKey().getTo(),
-                                annotationTokenListEntry.getKey().getText()
-                                        .toLowerCase())).append("\n");
-                        stringBuilder.append(String.format("#%d\tAnnotatorNotes T%d\t%s", termCounter, termCounter, buildCUILIst(annotatedClass.getCuis()))).append("\n");
-                    }
+        filteredAnnotations.sort(Comparator.comparingInt(QuaeroAnnotation::getFrom));
+        final List<QuaeroAnnotation> merged = mergeDuplicateTokenAnnotations(filteredAnnotations);
+        merged.sort(Comparator.comparingInt(QuaeroAnnotation::getFrom));
+
+        int termCounter = 0;
+        for (final QuaeroAnnotation annotation : merged) {
+            termCounter++;
+
+            final String group = annotation.semanticGroupLabel();
+            if (!group
+                    .trim()
+                    .isEmpty()) {
+                if (!ignoreAmbiguous || (annotation.semanticGroups.size() == 1)) {
+                    stringBuilder
+                            .append(String.format("T%d\t%s %d %d\t%s", termCounter, group,
+                                    annotation.getFrom(),
+                                    annotation.getTo(),
+                                    annotation
+                                            .getText()
+                                            .toLowerCase()))
+                            .append("\n");
+                    stringBuilder
+                            .append(String.format("#%d\tAnnotatorNotes T%d\t%s", termCounter, termCounter, annotation.cuiLabel()))
+                            .append("\n");
                 }
             }
         }
 
+
         return new LIRMMAnnotatorOutput(stringBuilder.toString(), MimeTypes.APPLICATION_BRAT);
     }
 
-    private String buildCUILIst(final Iterable<String> set) {
-        final StringBuilder stringBuilder = new StringBuilder("");
-        boolean first = true;
-        for (final String elem : set) {
-            if (!first) {
-                stringBuilder.append(" ");
-            }
-            stringBuilder.append(elem);
-            first = false;
-        }
-        return stringBuilder.toString();
+    private List<QuaeroAnnotation> mergeDuplicateTokenAnnotations(final Collection<QuaeroAnnotation> annotations) {
+        return annotations
+                .stream()
+                .collect(Collectors.groupingBy(QuaeroAnnotation::getSpan))
+                .values()
+                .stream()
+                .map(this::collapseList)
+                .collect(Collectors.toList());
     }
 
-    private String buildGroupList(final Iterable<UMLSGroup> set) {
-        final StringBuilder stringBuilder = new StringBuilder();
-        boolean first = true;
-        for (final UMLSGroup elem : set) {
-            if (!first) {
-                stringBuilder.append(",");
-            }
-            stringBuilder.append(elem.getName());
-            first = false;
+    @SuppressWarnings({"MethodReturnOfConcreteClass", "LocalVariableOfConcreteClass"})
+    private QuaeroAnnotation collapseList(final List<QuaeroAnnotation> annotations) {
+        final QuaeroAnnotation rootAnnotation = annotations.get(0);
+        final QuaeroAnnotation result = new QuaeroAnnotation(rootAnnotation.getFrom(), rootAnnotation.getTo(), rootAnnotation.text);
+        for (final QuaeroAnnotation quaeroAnnotation : annotations) {
+            result.addCUIs(quaeroAnnotation.cuis);
+            result.addSemanticGroups(quaeroAnnotation.semanticGroups);
         }
-        return stringBuilder.toString();
+        return result;
+    }
+
+    private static class QuaeroAnnotation {
+        final int from;
+        int to;
+        final String text;
+        final Set<String> semanticGroups;
+        final Set<String> cuis;
+
+        QuaeroAnnotation(final int from, final int to, final String text) {
+            this.from = from;
+            this.to = to;
+            this.text = text;
+            semanticGroups = new TreeSet<>();
+            cuis = new TreeSet<>();
+        }
+
+        void addUMLSSemanticGroups(final Collection<UMLSGroup> name) {
+            semanticGroups.addAll(name
+                    .stream()
+                    .map(UMLSGroup::getName)
+                    .collect(Collectors.toList()));
+        }
+
+        void addSemanticGroups(final Collection<String> name) {
+            semanticGroups.addAll(name);
+        }
+
+        void addCUIs(final Collection<String> cuis) {
+            this.cuis.addAll(cuis);
+        }
+
+        String semanticGroupLabel() {
+            return semanticGroups
+                    .stream()
+                    .collect(Collectors.joining(","));
+        }
+
+        String cuiLabel() {
+            return cuis
+                    .stream()
+                    .collect(Collectors.joining(" "));
+        }
+
+        public int getFrom() {
+            return from;
+        }
+
+        public int getTo() {
+            return to;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        Pair<Integer, Integer> getSpan() {
+            return Pair.of(from, to);
+        }
     }
 }
